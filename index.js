f15195a8fdb88e169ccf0fd8b3c86d3eaea01212
Binod SAPKOTA <u2382084@uel.ac.uk>
2024-12-20 02:55:57 +0000

    Initial commit with index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+console. log("This is my first program ");
+console. log( "Salary is 50000");
+    
+const num1= 3;
+const num2= 5;
+
+const sum = num1 + num2;
+const sub = num1 - num2;
+const div = num1/num2;
+const mul = num1*num2;
+
+console.log(' sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
+console.log(' sub of ' + num1 + ' and ' + num2 + ' is ' + sub);
+console.log(' div of ' + num1 + ' and ' + num2 + ' is ' + div);
+console.log(' mul of ' + num1 + ' and ' + num2 + ' is ' + mul);
+    
